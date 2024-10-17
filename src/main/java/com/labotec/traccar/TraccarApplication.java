e1281c1f36197759de69b28766cd09f1b41de1a1
gabo15@gmail.com <gabo15@gmail.com>
2024-10-17 11:45:46 -0500

    first commit

---
+++ src/main/java/com/labotec/traccar/TraccarApplication.java
@@ -0,0 +1,13 @@
+package com.labotec.traccar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraccarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraccarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labotec/traccar/TraccarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labotec.traccar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraccarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
